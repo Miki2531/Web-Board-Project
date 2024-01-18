2daff6b2ebe833c45181faa65f85d96970700652
Miki2531 <mikiyasshemelis46@gmail.com>
2024-01-18 17:06:54 +0300

    creating views for new topic function

--- board/board_data/views.py
+++ board/board_data/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import *
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect,  get_object_or_404
+from .models import Board, Topic, Post
 
 # Create your views here.
 
@@ -17,7 +17,30 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    context = {'baord': board}
+
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board = board,
+            starter = user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic = topic,
+            created_by = user
+        )
+
+        return redirect('board_topics', pk=board.pk)
+
+    context = {'board': board}
     return render(request, 'new_topic.html', context)
 
 
