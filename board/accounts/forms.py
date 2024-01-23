d5b73f513c0e57ac8a7ad8f63009461d7ada48db
Miki2531 <mikiyasshemelis46@gmail.com>
2024-01-23 15:55:56 +0300

    creating views, urls, forms for login, logout and signup page.

--- board/accounts/forms.py
+++ board/accounts/forms.py
@@ -7,3 +7,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model =User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
--- board/accounts/views.py
+++ board/accounts/views.py
@@ -1,16 +1,54 @@
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm 
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import SignUpForm
+
+def login_views(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+
+        except:
+            messages.error(request, 'user name does not exists.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(request.GET['next'] if 'next' in request.GET else 'home')
+        else:
+            messages.error(request, 'Username or password is incorrect!!')
+
+    context = {'page': page}
+    return render(request, 'login.html', context)
+
+
+
+
+def logout_views(request):
+    logout(request)
+    return redirect('home')
+
+
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            auth_login(request, user)
+            login(request, user)
             return redirect('home')
     else: 
         form = SignUpForm()
     context = {'form': form}
     return render (request, 'signup.html', context)
+
