ab4f1fd68da0767cf85c0354c1d1421e6595492f
Miki2531 <mikiyasshemelis46@gmail.com>
2024-01-23 17:24:28 +0300

    testing creating user accounts

--- board/board/urls.py
+++ board/board/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from board_data import views
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('board_data.urls')),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
 ]
