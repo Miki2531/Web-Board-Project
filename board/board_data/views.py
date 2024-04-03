82182488e23a464cf58e5317479097171f9b719f
Miki2531 <mikiyasshemelis46@gmail.com>
2024-04-03 09:17:42 +0300

    Add Edit post features user to edit thier post.

--- board/board_data/views.py
+++ board/board_data/views.py
@@ -1,52 +1,91 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect,  get_object_or_404
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
+from django.db.models import Count
 from .models import Board, Topic, Post
+from django.utils import timezone
+from django.views.generic import UpdateView
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk = post.topic.board.pk, topic_pk = post.topic.pk)
+
 
 # Create your views here.
-
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
     
-
+@login_required
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    context = {'board': board}
+    topics = board.topics.order_by('-last_update').annotate(replies=Count('posts') - 1)
+    context = {'board': board, 'topic' : topics}
     return render(request, 'topics.html', context)
 
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()
-
-
+ 
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
 
             post = Post.objects.create(
                 message = form.cleaned_data.get('message'),
                 topic = topic,
-                created_by = user
+                created_by = request.user
             )
 
-        return redirect('board_topics', pk=board.pk)
+        return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
 
     context = {'board': board, 'form': form}
     return render(request, 'new_topic.html', context)
 
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views +=1
+    topic.save()
+    context = {'topic': topic}
+    return render(request, 'topic_posts.html', context)
 
 
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
 
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
 
-
+    else:
+        form = PostForm()
+    context = {'topic': topic, 'form': form}
+    return render(request, 'reply_topic.html',  context)
 
     
 
