6872d37e54ae472bf3628746abe68ecf3ffa9f50
Miki2531 <mikiyasshemelis46@gmail.com>
2024-04-12 09:52:19 +0300

    Remove secret key from seeting and store in .env file

--- board/board/settings.py
+++ board/board/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config, Csv
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-02^8g!uzinxie4x5n3=1%(75h#+6mizjs+%=(f9vn)e+2rn=7&'
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default' : dj_database_url.config(
+        default = config('DATABASE_URL')
+    )
+}
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
